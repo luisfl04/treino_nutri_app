565b834b6b21867db9994827ba60cd9f3b3037c8
luis felipe <luisfelipecontato08@gmail.com>
2026-05-26 15:14:47 -0300

    fix: Resolvendo integracao entre branchs

--- app/lib/firestore/FirestoreService.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class FirestoreService {
-  static final FirestoreService _instance = FirestoreService._internal();
-  factory FirestoreService() => _instance;
-  FirestoreService._internal();
-  FirebaseFirestore get db => FirebaseFirestore.instance;
-}
